a6e24be260ac05c12ccf8b605bed6a135d47c973
collinspg <collinsokolie90@gmail.com>
2025-07-22 21:59:05 +0100

    added model task

---
+++ models/task.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true },
+  description: String,
+  dueDate: Date,
+  assignedUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  completed: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
